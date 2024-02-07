1ac50b25819d0a4dcc88fa70dce14e8dc4bb9185
nvminh09 <nvminh09@gmail.com>
2024-02-08 01:13:59 +0700

    Remove comment Firebase.initializeApp in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'authentication/login_screen.dart';
 Future<void> main() async
 {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
+  await Firebase.initializeApp();
 
   await Permission.locationWhenInUse.isDenied.then((valueOfPermission)
   {
