8c93f8bcb7728f6a17345d30f947f49d66413f76
nvminh09 <nvminh09@gmail.com>
2024-02-19 13:56:45 +0700

    update dashboard.dart

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -8,8 +8,34 @@ class Dashboard extends StatefulWidget
   State<Dashboard> createState() => _DashboardState();
 }
 
-class _DashboardState extends State<Dashboard>
+class _DashboardState extends State<Dashboard> with SingleTickerProviderStateMixin
 {
+  TabController? controller;
+  int indexSelected = 0;
+
+  onBarItemClicked(int i)
+  {
+    setState(() {
+      indexSelected = i;
+      controller!.index = indexSelected;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    controller = TabController(length: 4, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+
   @override
   Widget build(BuildContext context)
   {
