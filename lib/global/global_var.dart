bc6fc7d413ebef4d08e2e2a0915ed9e13e40caef
nvminh09 <nvminh09@gmail.com>
2024-04-08 00:43:26 +0700

    Newest

--- lib/global/global_var.dart
+++ lib/global/global_var.dart
@@ -2,9 +2,9 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 String userName = "";
 
-String googleMapKey = "AIzaSyB8cfSNTxbXDnnikm7maw9KJJZRPca8gJw";
+String googleMapKey = "AIzaSyDuDxriw8CH8NbVLiXtKFQ2Nb64AoRSdyg";
 
 const CameraPosition googlePlexInitialPosition = CameraPosition(
-  target: LatLng(37.42796133580664, -122.085749655962),
+  target: LatLng(10.798879771962616, 106.66463381075194),
   zoom: 14.4746,
 );
