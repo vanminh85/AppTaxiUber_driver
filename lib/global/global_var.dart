bed2e302005d801b2f206db5b7a318efe327feec
nvminh09 <nvminh09@gmail.com>
2024-04-22 02:28:44 +0700

    last for driver app

--- lib/global/global_var.dart
+++ lib/global/global_var.dart
@@ -1,7 +1,8 @@
 import 'dart:async';
-
+import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 
 String userName = "";
 
@@ -13,3 +14,17 @@ const CameraPosition googlePlexInitialPosition = CameraPosition(
 );
 
 StreamSubscription<Position>? positionStreamHomePage;
+StreamSubscription<Position>? positionStreamNewTripPage;
+
+int driverTripRequestTimeout = 20;
+
+final audioPlayer = AssetsAudioPlayer();
+
+Position? driverCurrentPosition;
+
+String driverName = "";
+String driverPhone = "";
+String driverPhoto = "";
+String carColor = "";
+String carModel = "";
+String carNumber = "";
